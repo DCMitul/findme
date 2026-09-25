06f01b4f53ca967c03facc342427203e6055ca96
DCMitul <105763249+DCMitul@users.noreply.github.com>
2026-09-25 23:54:46 +0530

    Implemented the code for initializing the config and the db

--- python/main.py
+++ python/main.py
@@ -1,11 +1,66 @@
 import pathlib as pth
 import os
+import json
+import sys
+import sqlite3
+
 
 try:
     from InquirerPy import inquirer
 except ImportError:
     print("The library 'InquirerPy' is not present, install it using 'pip install InquirerPy' to proceed.")
     sys.exit()
+
+
+# The function that initializes the config and stuff 
+def inicnf():
+    if not os.path.exists(CONFIGPATH):
+        temp = {
+            "db": str(FINDME / "data.db"),
+        }
+
+        with open(CONFIGPATH, "w") as f:
+            json.dump(temp, f, indent=4)
+        
+        return(True)
+    else:
+        return(True)    
+
+
+# The function that makes reading and writing to config a whole lot easier
+def hconfig(action,key,value=None):
+    if inicnf():
+        with open(CONFIGPATH, "r") as f:
+            temp = json.load(f)
+
+        if action == 'read':
+            return temp.get(key)
+        elif action == 'change':
+            temp[key] = value
+            with open(CONFIGPATH, "w") as f:
+                json.dump(temp, f, indent=4)
+        else:
+            print("invalid input for action", action)
+
+
+# The function the initalizes the database
+def inidb():
+    conn = sqlite3.connect(hconfig('read','db'))
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            path TEXT NOT NULL,
+            terms TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+    return True
+
+
+# The function that manages writing stuff to the db
 
 
 # The two functions that make it easier to get input or confirm smth
@@ -60,7 +115,7 @@ def getfiles(dir):
     for root, b, files in os.walk(dir,onerror=lambda error: None):
         for file in files:
             if checkfile(file):
-                temp.append(os.path.join(root, file))
+                temp.append(os.path.normpath(os.path.join(root, file)))
 
 
     return temp
@@ -82,7 +137,10 @@ def index():
 EXTENTIONS = [".txt",".md"]
 FINDME = pth.Path(__file__).resolve().parent.resolve().parent
 # print(FINDME)                                   # Works fine :>
+CONFIGPATH = FINDME / "config.json"
 
+
+inidb()
 index()
 
 
